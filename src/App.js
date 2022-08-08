cdb20f63f074053cbe46c8d35e25c138725b0d41
lrpineda <luicks212@gmail.com>
2022-08-08 12:47:23 -0600

    Mantine library setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Footer,
+  Aside,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+  MantineProvider,
+  Paper,
+  
+} from "@mantine/core";
+import Demo from './demo';
 
+import { useColorScheme } from "@mantine/hooks";
+import { useState } from "react";
 function App() {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+  const preferredColorScheme = useColorScheme();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <MantineProvider theme={{ colorScheme: preferredColorScheme }} withGlobalStyles withNormalizeCSS>
+        <Paper>
+          <AppShell
+          styles={{
+            main: {
+              background:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[8]
+                  : theme.colors.gray[0],
+            },
+          }}
+          navbarOffsetBreakpoint="sm"
+          asideOffsetBreakpoint="sm"
+          navbar={
+            <Navbar
+              p="md"
+              hiddenBreakpoint="sm"
+              hidden={!opened}
+              width={{ sm: 200, lg: 300 }}
+            >
+              <Text>Application navbar</Text>
+            </Navbar>
+          }
+          aside={
+            <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
+              <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
+                <Text>Application sidebar</Text>
+              </Aside>
+            </MediaQuery>
+          }
+          footer={
+            <Footer height={60} p="md">
+              Application footer
+            </Footer>
+          }
+          header={
+            <Header height={70} p="md">
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
+                <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                  <Burger
+                    opened={opened}
+                    onClick={() => setOpened((o) => !o)}
+                    size="sm"
+                    color={theme.colors.gray[6]}
+                    mr="xl"
+                  />
+                </MediaQuery>
+
+                <Text>Application header</Text>
+              </div>
+            </Header>
+          }
+          >
+            <Text>Application content</Text>
+
+          </AppShell>
+        </Paper>  
+      </MantineProvider>
+
   );
 }
 
